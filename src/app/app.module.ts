4423b53df2134943dec6665385fe2f1597843ee8
Ankitha Nair <ankithanair200028@gmail.com>
2021-12-23 23:21:58 +0530

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+<<<<<<< Updated upstream
 import { NavbarComponent } from './navbar/navbar.component';
+=======
+import { FormsModule } from '@angular/forms';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     LoginComponent,
     NavbarComponent
+=======
+    LoginComponent
+
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(){
+    console.log("App Component Loaded..");
+  }
+ }
